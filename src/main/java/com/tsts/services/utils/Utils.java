8929114462acd87fb670d37f4fd149fc74522ef7
TayssirGh <tayssir.ghanmi@eniso.u-sousse.tn>
2024-12-07 22:17:53 +0100

    [UPDATED] fixed some issues concerning add command

--- src/main/java/com/tsts/services/utils/Utils.java
+++ src/main/java/com/tsts/services/utils/Utils.java
@@ -2,7 +2,9 @@ package com.tsts.services.utils;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Utils {
     private static final String CONFIG_FILE = System.getProperty("user.home") + "/histograph";
@@ -36,12 +38,26 @@ public class Utils {
 
 
     public static void saveRepositories(List<String> repositories) {
+        Set<String> existingRepositories = new HashSet<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(CONFIG_FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                existingRepositories.add(line.trim());
+            }
+        } catch (IOException e) {
+            System.err.println("Failed to read existing repositories: " + e.getMessage());
+        }
+
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(CONFIG_FILE, true))) {
             for (String repo : repositories) {
-                writer.write(repo);
-                writer.newLine();
-                System.out.println("Repositories added successfully.");
-
+                if (!existingRepositories.contains(repo.trim())) {
+                    writer.write(repo);
+                    writer.newLine();
+                    System.out.println("Repository added successfully ✅: ");
+                } else {
+                    System.out.println("Repository already exists: ");
+                }
             }
         } catch (IOException e) {
             System.err.println("Failed to save repositories: " + e.getMessage());
